cceb4d80c9301841eb2a5590305eecea6d50f305
Staffan <enemark51@hotmail.com>
2018-05-29 11:21:20 +0200

    Change comment

--- resources/assets/js/components/TaskComponent.vue
+++ resources/assets/js/components/TaskComponent.vue
@@ -112,7 +112,6 @@
         methods: {
             getTasks: function () {
                 var self = this;
-                // Make a request for a user with a given ID
                 axios.get('/api/tasks/all')
                     .then(function (response) {
                         self.tasks = response.data.tasks;
